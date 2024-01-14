a95788e79e237363ff02ac12bc43afb00c3b995e
U-235-92 <a745296@yandex.ru>
2024-01-14 16:46:44 +0400

    -C

--- other_consumer_ejb/src/main/java/com/other/main/MainHelloConsumer.java
+++ other_consumer_ejb/src/main/java/com/other/main/MainHelloConsumer.java
@@ -1,4 +1,4 @@
-package com.other.consumer;
+package com.other.main;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -6,14 +6,12 @@ import javax.naming.NamingException;
 
 import com.other.hello.HelloEJBRemote;
 
-//doesn't work
-public class MainHello {
+public class MainHelloConsumer {
 
 	public static void main(String[] args) throws NamingException {
 		Context context = new InitialContext();
 		HelloEJBRemote helloEJB = (HelloEJBRemote) 
-				context.lookup("java:global/other_hello_ejb-0.0.1-SNAPSHOT/HelloEJB!com.other.hello.HelloEJBRemote"); 
+				context.lookup("java:global/other_hello_ejb-0.0.1-SNAPSHOT/HelloEJB!com.other.hello.HelloEJBRemote");
 		helloEJB.sayHello();
 	}
-
 }
