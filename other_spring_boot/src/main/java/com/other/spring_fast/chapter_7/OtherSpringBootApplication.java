5156cec8f699670daf8c46beadb60cace068aefe
U-235-92 <a745296@yandex.ru>
2024-03-18 18:08:27 +0400

    Spring fast chapter 7

---
+++ other_spring_boot/src/main/java/com/other/spring_fast/chapter_7/OtherSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.other.spring_fast.chapter_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherSpringBootApplication.class, args);
+	}
+
+}
---
+++ other_spring_boot/src/test/java/com/other/spring_fast/chapter_7/OtherSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other.spring_fast.chapter_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
