47b763b7511be394ba885daa1a0c964ae20acb57
U-235-92 <a745296@yandex.ru>
2024-03-21 18:40:15 +0400

    Spring fast chapter 10 in progress

---
+++ other_spring_fast_chapter_10/src/main/java/com/other/spring_fast/chapter_10/rest/OtherSpringFastChapter10Application.java
@@ -0,0 +1,13 @@
+package com.other.spring_fast.chapter_10.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherSpringFastChapter10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherSpringFastChapter10Application.class, args);
+	}
+
+}
---
+++ other_spring_fast_chapter_10/src/test/java/com/other/spring_fast/chapter_10/OtherSpringFastChapter10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other.spring_fast.chapter_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherSpringFastChapter10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
