333c2c5a22b08b11c76aa3738e705802c4c16644
U-235-92 <a745296@yandex.ru>
2024-03-27 18:46:05 +0400

    Spring fast chapter 12

--- other_spring_fast_chapter_12/src/main/java/com/other/spring_fast/chapter_12/db/main/Configuration.java
+++ other_spring_fast_chapter_12/src/main/java/com/other/spring_fast/chapter_12/db/main/Configuration.java
@@ -1,9 +1,31 @@
 package com.other.spring_fast.chapter_12.db.main;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @org.springframework.context.annotation.Configuration
 @ComponentScan(basePackages = {"com.other.spring_fast.chapter_12.db.repository", "com.other.spring_fast.chapter_12.db.controller"})
 public class Configuration {
 
+	@Value("${custom.datasource.url}")
+	private String datasourceUrl;
+	@Value("${custom.datasource.username}")
+	private String datasourceUsername;
+	@Value("${custom.datasource.password}")
+	private String datasourcePassword;
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(datasourceUrl);
+		dataSource.setUsername(datasourceUsername);
+		dataSource.setPassword(datasourcePassword);
+		dataSource.setConnectionTimeout(10000);
+		return dataSource;
+	}
 }
