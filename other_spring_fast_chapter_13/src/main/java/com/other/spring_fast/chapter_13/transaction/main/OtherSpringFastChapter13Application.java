2698c7142d0d647beebc717a631e9c1f8a8c76fa
U-235-92 <a745296@yandex.ru>
2024-03-28 19:46:32 +0400

    Spring fast chapter 13 work in progress

---
+++ other_spring_fast_chapter_13/src/main/java/com/other/spring_fast/chapter_13/transaction/main/OtherSpringFastChapter13Application.java
@@ -0,0 +1,13 @@
+package com.other.spring_fast.chapter_13.transaction.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherSpringFastChapter13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherSpringFastChapter13Application.class, args);
+	}
+
+}
---
+++ other_spring_fast_chapter_13/src/test/java/com/other/spring_fast/chapter_13/transaction/main/OtherSpringFastChapter13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other.spring_fast.chapter_13.transaction.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherSpringFastChapter13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
